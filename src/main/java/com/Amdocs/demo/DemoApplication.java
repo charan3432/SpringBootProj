25ffe8ce92fb9e89952b8dd17203239914545824
Mahendrakar Charan <charanmahendrakar44@gmail.com>
2024-07-31 14:48:57 +0530

    first commit

---
+++ src/main/java/com/Amdocs/demo/DemoApplication.java
@@ -0,0 +1,22 @@
+package com.Amdocs.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+@SpringBootApplication
+public class DemoApplication {
+	
+	
+	@GetMapping("/hello")
+	public String Demo()
+	{
+		return "Welcome";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
+
